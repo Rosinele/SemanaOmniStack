23eacd2cbc8c3618e961e0efa1880434043dd87a
rosinele <rosefelix99@gmail.com>
2020-03-24 05:23:57 -0300

    Hello word com Node e React

--- aulas/back-end/index.js
+++ aulas/back-end/index.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (request, response) =>{
+    return response.json({
+        evento: 'Semana OmniStack 1.0',
+        aluno: 'Rose Félix'
+    });
+});
+
 app.listen(3333);
