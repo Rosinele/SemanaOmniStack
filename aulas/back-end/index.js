d60e5b5eb77fb92d15900b7d0a399f5b2447c547
rosinele <rosefelix99@gmail.com>
2020-04-02 00:56:28 -0300

    tipos de parametros

--- aulas/back-end/index.js
+++ aulas/back-end/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (request, response) =>{
+
+app.use(express.json());
+ 
+app.post('/users/', (request, response) =>{
+    // query params const params = request.query;
+    // route params const params = request.params;
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
         evento: 'Semana OmniStack 1.0',
         aluno: 'Rose Félix'
