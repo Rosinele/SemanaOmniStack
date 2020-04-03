f69b0b215aca49f9dc7009a27a0f281d4362b021
rosinele <rosefelix99@gmail.com>
2020-04-03 05:35:15 -0300

    inserção de dados nas tabelas criadas

--- aulas/back-end/src/routes.js
+++ aulas/back-end/src/routes.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users/', (request, response) =>{
+routes.post('/ongs', async (request, response) => {
     // query params const params = request.query;
     // route params const params = request.params;
-    const body = request.body;
+    // request body const body = request.body;
 
-    console.log(body);
+    const { name, email, whatsapp, city, uf } = request.body;
 
-    return response.json({
-        evento: 'Semana OmniStack 1.0',
-        aluno: 'Maria Rose Félix do Nascimento'
-    });
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+    return response.json({ id });
 });
 
 module.exports = routes;
