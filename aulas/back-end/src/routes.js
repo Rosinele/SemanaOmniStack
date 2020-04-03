59a9108ee88a3f457e0e418ed4206e2687c8b125
rosinele <rosefelix99@gmail.com>
2020-04-03 06:46:18 -0300

    separação dos arquivos de rotas em arquivos controller

--- aulas/back-end/src/routes.js
+++ aulas/back-end/src/routes.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-
-    return response.json(ongs);
-});
-
-routes.post('/ongs', async (request, response) => {
-    // query params const params = request.query;
-    // route params const params = request.params;
-    // request body const body = request.body;
-
-    const { name, email, whatsapp, city, uf } = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-    return response.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
