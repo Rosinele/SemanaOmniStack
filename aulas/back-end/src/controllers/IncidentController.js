5a1abd7c40705743240de09c0f3c495909f9916d
rosinele <rosefelix99@gmail.com>
2020-04-03 11:57:18 -0300

    Mostrar contador de itens no header

--- aulas/back-end/src/controllers/IncidentController.js
+++ aulas/back-end/src/controllers/IncidentController.js
@@ -4,11 +4,14 @@ module.exports = {
     async index(request, response) {
         const {page = 1 } = request.query;
 
+        const [count] = await connection('incidents').count();
+
+        console.log(count);
         const incidents = await connection('incidents')
             .limit(5)
             .offset((page -1)*5)
             .select('*');
-
+        response.header('X-Total-Count', count['count(*)']);
         return response.json(incidents);
     },
 
