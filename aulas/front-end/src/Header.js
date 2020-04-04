33ada5899e5a91ca93b33951576221bc74b744da
rosinele <rosefelix99@gmail.com>
2020-04-04 02:34:10 -0300

    Estados e imutabilidade no react

--- aulas/front-end/src/Header.js
+++ aulas/front-end/src/Header.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Header({children}){
+    const [counter, setCounter]= useState(0);
+
+    function increment(){
+        setCounter(counter + 1);
+    }
+
     return(
-        <header>
-            <h1>{props.children}</h1>
-        </header>
+        <div>
+            <header>Contador:{counter}</header>
+            <button onClick={increment}>Incrementar</button>
+        </div>
+        
     )
 }
 
